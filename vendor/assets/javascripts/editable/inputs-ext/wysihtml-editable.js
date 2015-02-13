dedab9e2833106011b86468d75134eba40f16ca0
Rafael Souza <rafael.ssouza@gmail.com>
2015-02-13 14:01:37 -0200

    add heading format buttons and use custom buttons for save/cancel

--- vendor/assets/javascripts/editable/inputs-ext/wysihtml-editable.js
+++ vendor/assets/javascripts/editable/inputs-ext/wysihtml-editable.js
@@ -1,42 +1,45 @@
-var wysihtml5ParserRules = {
-  tags: {
-    strong: {},
-    b:      {},
-    i:      {},
-    em:     {},
-    u:      {},
-    br:     {},
-    p:      {},
-    div:    {},
-    span:   {},
-    ul:     {},
-    ol:     {},
-    li:     {},
-    a:      {
-        set_attributes: {
-            target: "_blank",
-            rel:    "nofollow"
-        },
-        check_attributes: {
-            href:   "url" // important to avoid XSS
-        }
-    },
-    img: {
-        check_attributes: {
-            width: "dimension",
-            alt: "alt",
-            src: "url", // if you compiled master manually then change this from 'url' to 'src'
-            height: "dimension"
-        },
-        add_class: {
-            align: "align_img"
-        }
-    },
-  }
-};
-
 (function ($) {
     "use strict";
+
+    var wysihtml5ParserRules = {
+      tags: {
+        strong: {},
+        b: {},
+        i: {},
+        em: {},
+        u: {},
+        br: {},
+        p: {},
+        div: {},
+        span: {},
+        ul: {},
+        ol: {},
+        li: {},
+        h1: {},
+        h2: {},
+        h3: {},
+        a: {
+            set_attributes: {
+                target: "_blank",
+                rel: "nofollow"
+            },
+            check_attributes: {
+                href: "url" // important to avoid XSS
+            }
+        },
+        img: {
+            check_attributes: {
+                width: "dimension",
+                alt: "alt",
+                src: "url", // if you compiled master manually then change this from 'url' to 'src'
+                height: "dimension"
+            },
+            add_class: {
+                align: "align_img"
+            }
+        },
+      }
+    };
 
     var Wysihtml = function (options) {
         this.init('wysihtml', options, Wysihtml.defaults);
@@ -92,15 +95,27 @@ var wysihtml5ParserRules = {
         tpl: '<div class="toolbar">' +
             '<div class="btn-toolbar">' +
                 '<div class="btn-group">' +
-                    '<a class="btn" unselectable="on" href="javascript:;" data-wysihtml5-command="bold"><strong>Bold</strong></a>' +
-                    '<a class="btn" unselectable="on" href="javascript:;" data-wysihtml5-command="italic"><i>Italic</i></a>' +
-                    '<a class="btn" unselectable="on" href="javascript:;" data-wysihtml5-command="underline"><u>Underline</u></a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="formatBlock" data-wysihtml5-command-blank-value="true">Normal</a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1">Heading 1</a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2">Heading 2</a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h3">Heading 3</a>' +
                 '</div>' +
                 '<div class="btn-group">' +
-                    '<a class="btn" unselectable="on" href="javascript:;" data-wysihtml5-command="createLink"><i class="icon-share"></i></a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="bold"><strong>Bold</strong></a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="italic"><i>Italic</i></a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="underline"><u>Underline</u></a>' +
                 '</div>' +
                 '<div class="btn-group">' +
-                    '<a class="btn" unselectable="on" href="javascript:;" data-wysihtml5-command="insertImage"><i class="icon-picture"></i></a>' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="createLink"><i class="icon-share"></i></a>' +
+                '</div>' +
+                '<div class="btn-group">' +
+                    '<a class="btn btn-small" unselectable="on" href="javascript:;" data-wysihtml5-command="insertImage"><i class="icon-picture"></i></a>' +
+                '</div>' +
+                '<div class="btn-group pull-right">' +
+                    '<button type="submit" class="btn btn-small editable-cancel"><i class="icon-remove"></i></button>' +
+                '</div>' +
+                '<div class="btn-group pull-right">' +
+                    '<button type="submit" class="btn btn-primary btn-small editable-submit"><i class="icon-ok icon-white"></i></button>' +
                 '</div>' +
             '</div>' +
             '<div data-wysihtml5-dialog="createLink" class="form-inline" style="display:none;">' +
@@ -116,22 +131,10 @@ var wysihtml5ParserRules = {
         '</div>' +
         '<div class="editor"></div>',
         inputclass: 'editable-wysihtml',
+        showbuttons: false,
         wysihtml: {
         }
     });
 
     $.fn.editabletypes.wysihtml = Wysihtml;
 }(window.jQuery));
-
-
-
-// <div class="btn-group">
-//     <a unselectable="on" href="javascript:;" class="btn" data-wysihtml5-command="insertUnorderedList" title="Unordered list" tabindex="-1">
-//         <i class="icon-list"></i>
-//     </a>
-//     <a unselectable="on" href="javascript:;" class="btn" data-wysihtml5-command="insertOrderedList" title="Ordered list" tabindex="-1"><i class="icon-th-list"></i></a><a unselectable="on" href="javascript:;" class="btn" data-wysihtml5-command="Outdent" title="Outdent" tabindex="-1"><i class="icon-indent-right"></i></a>
-//     <a unselectable="on" href="javascript:;" class="btn" data-wysihtml5-command="Indent" title="Indent" tabindex="-1">
-//         <i class="icon-indent-left">
-//         </i>
-//     </a>
-// </div>
