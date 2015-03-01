4f37f2841ddc3b7c69e175ee6cedcb994361df34
Smolev Denis <smolev@me.com>
2015-03-01 21:40:34 +0300

    comment

--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -113,7 +113,7 @@ module X
           if source && ( source.first.is_a?(String) || source.kind_of?(Hash) )
             values.map{|item| source[output_value_for item]}
           elsif source && source.first.is_a?(Hash)
-            #example source: User.all.map{|u| {id: u.id.to_s, text: u.public_name}}
+            #example source: User.all.map{|u| {id: u.id, text: u.public_name}}
             source.detect {|f| f["id"] == value }.values-[value]
           else
             values
